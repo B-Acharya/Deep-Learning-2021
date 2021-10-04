2c70feb9bec49a2a6669116b85d64c0ae7234074
Bhargav Acharya <bhargav.acharya@uni-bielefeld.de>
2021-10-04 11:30:35 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projects for the class deep learning 2021.',
+    author='Team_28',
+    license='',
+)
