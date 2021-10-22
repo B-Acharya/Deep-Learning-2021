2c10c1eeae8242c56fb0e86a76ff25c5f822b409
bhargav28 <bhargav281996@gmail.com>
2021-10-22 14:52:55 +0200

    added varconv

--- src/models/variational_encoder.py
+++ src/models/variational_encoder.py
@@ -19,6 +19,15 @@ import time
 from torch.utils.data import DataLoader
 import sys 
 # 
+#from https://github.com/dstallmann/cell_cultivation_analysis/blob/1ef9c0e11e05200d672323f604674c3bc9e5e016/vae/VAE.py#L39
+class UnFlatten(nn.Module):
+	def forward(self, data):
+		"""
+		Unflattens the network at the beginning of the decoder
+		@param data: data to be unflattened
+		@return: unflattened data
+		"""
+		return data.view(data.size(0), -1, 1, 1)
 
 
 # code adapted form https://avandekleut.github.io/vae/
@@ -116,23 +125,31 @@ class VariationalEncoderConv(nn.Module):
         self.encoder_1 = nn.Sequential(
             nn.Conv2d(in_channels= 1, out_channels=6, kernel_size=5, stride=1),
             nn.LeakyReLU(),
+            nn.Dropout(),
             nn.MaxPool2d(kernel_size=2),
             nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0, bias=True),
+            nn.LeakyReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Flatten(),
-            nn.Linear(16*5*5, 120),
+            nn.Linear(256, 120),
+            nn.LeakyReLU(),
             nn.Linear(120, 84),
+            nn.LeakyReLU(),
         )
 
         self.encoder_2 = nn.Sequential(
             nn.Conv2d(in_channels= 1, out_channels=6, kernel_size=5, stride=1),
             nn.LeakyReLU(),
+            nn.Dropout(),
             nn.MaxPool2d(kernel_size=2),
             nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0, bias=True),
+            nn.LeakyReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Flatten(),
-            nn.Linear(16*5*5, 120),
+            nn.Linear(256, 120),
+            nn.LeakyReLU(),
             nn.Linear(120, 84),
+            nn.LeakyReLU(),
         )
 
         #calculate mean|
@@ -145,10 +162,10 @@ class VariationalEncoderConv(nn.Module):
 
     def forward(self, x):
         x1 = x[0]
-        x1 = self.encoder_1(x1) 
+        x1 = self.encoder_1(x1.unsqueeze(0)) 
         #second brach 
         x2 = x[1]
-        x2 = self.encoder_2(x1) 
+        x2 = self.encoder_2(x2.unsqueeze(0)) 
         #second input layer
         #calculate the mean 
         # print(x1.shape, x2.shape)
@@ -161,6 +178,7 @@ class VariationalEncoderConv(nn.Module):
         z = mu + sigma*self.N.sample(mu.shape)
         # kl divergance for the latent space distribution and guassian normal distribution with mean = 0 and varience = 1 
         self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
+        # print(z.shape)
         return z
 
 class Decoder(nn.Module) :
@@ -185,22 +203,26 @@ class DecoderConv(nn.Module) :
     def __init__(self, latentDims):
         super().__init__()
         #simple decoder layer
-        self.linear1 = nn.Linear(latentDims, 256)
+        self.linear1 = nn.Linear(latentDims, 128)
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(256, 256, kernel_size=5, stride=2),
+            UnFlatten(),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2),
+            nn.BatchNorm2d(64),
             nn.LeakyReLU(),
-            nn.ConvTranspose2d(256, 128, kernel_size=5, stride=2),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2),
+            nn.BatchNorm2d(32),
             nn.LeakyReLU(),
-            nn.ConvTranspose2d(128,64, kernel_size=5, stride=2),
+            nn.ConvTranspose2d(32, 8, kernel_size=2, stride=1),
+            nn.BatchNorm2d(8),
             nn.LeakyReLU(),
-            nn.ConvTranspose2d(64,32, kernel_size=5, stride=2),
-            nn.LeakyReLU(),
+            nn.Flatten(),
+            nn.Linear(968, 784),
             nn.Sigmoid(),
         ) 
 
     def forward(self, z):
-
         z = F.relu(self.linear1(z))
+        # print(z.shape)
         #TODO why sigmoid ? 
         z = self.decoder(z)
         return z.reshape(-1,1,28,28)
@@ -219,10 +241,25 @@ class VariationalAutoEncoder(nn.Module):
         z = self.encoder(x)
         return self.decoder(z)
 
+class VariationalAutoEncoderConv(nn.Module):
+
+    def __init__(self, latentDims) :
+        super().__init__()
+
+        self.encoder = VariationalEncoderConv(latentDims)
+        self.decoder = DecoderConv(latentDims)
+
+
+    def forward(self, x):
+
+        z = self.encoder(x)
+        return self.decoder(z)
+
 def train(model, epochs, optimizer, dataloader, output):
     # TODO : compatibility to GPU
     losses =[]
     total_loss = 0
+    loss_hist = []
     for epoch in range(epochs):
         for images , labels in tqdm(dataloader):
             optimizer.zero_grad()
@@ -233,8 +270,10 @@ def train(model, epochs, optimizer, dataloader, output):
             loss.backward()
             optimizer.step()
             total_loss += loss.item()
+            loss_hist.append(loss.item())
         total_loss = total_loss/len(dataloader)
-        losses.append(total_loss)
+        losses.append(loss_hist)
+        loss_hist = []
         print(f"loss: {total_loss:>7f} ")
         total_loss = 0
         torch.save(model.state_dict(), save_path + f"\model{epoch}.pth")
@@ -244,11 +283,11 @@ if __name__=="__main__":
     sys.path.insert(1, "D:\Deep-Learning-2021\Deep-Learning-2021")
     from src.data.load_mnist import MNIST 
     latentDims = 10
-    epochs = 200
+    epochs = 40
     learning_rate = 1e-7
     save_path = "D:\Deep-Learning-2021\Deep-Learning-2021\models"
     mnist_trainset = MNIST(root= "D:\Deep-Learning-2021\Deep-Learning-2021\src\data", train=True, transform = torchvision.transforms.ToTensor())
-    vae = VariationalAutoEncoder(latentDims)
+    vae = VariationalAutoEncoderConv(latentDims)
     opt = torch.optim.Adam(vae.parameters(), lr=learning_rate)
     train_loader = DataLoader(mnist_trainset, batch_size= 2 ) 
     sum_images = mnist_trainset.get_examples()
